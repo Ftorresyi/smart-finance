88fb23f25796377e90b8d787b4a4ed38114dc89f
2S - YINDI <yindiyft@ccarj.intraer>
2025-07-01 15:51:54 -0300

    Implementaçao do User Registre com Autenticacao Spring Security

--- src/main/java/Finance/organizador_financeiro/service/UserService.java
+++ src/main/java/Finance/organizador_financeiro/service/UserService.java
@@ -1,8 +1,11 @@
 package Finance.organizador_financeiro.service;
 
+import Finance.organizador_financeiro.domain.User;
+import Finance.organizador_financeiro.dto.RegisterDTO;
+import Finance.organizador_financeiro.dto.UserDTO;
 import Finance.organizador_financeiro.mapper.UserMapper;
 import Finance.organizador_financeiro.repository.UserRepository;
-import Finance.organizador_financeiro.dto.UserDTO;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,10 +16,26 @@ import java.util.stream.Collectors;
 public class UserService {
     private final UserRepository repository;
     private final UserMapper mapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository repository, UserMapper mapper) {
+    public UserService(UserRepository repository, UserMapper mapper, PasswordEncoder passwordEncoder) {
         this.repository = repository;
         this.mapper = mapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registerNewUser(RegisterDTO registerDTO) {
+        if (repository.findByEmail(registerDTO.getEmail()).isPresent()) {
+            throw new IllegalStateException("E-mail já cadastrado.");
+        }
+
+        User newUser = User.builder()
+                .name(registerDTO.getName())
+                .email(registerDTO.getEmail())
+                .password(passwordEncoder.encode(registerDTO.getPassword()))
+                .build();
+
+        repository.save(newUser);
     }
 
     public List<UserDTO> findAll() {
